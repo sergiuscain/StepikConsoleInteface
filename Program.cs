30ddba13d8e2716ca78d29ae2b721a6bb34745fb
Sergey Korolev <sergiussmithe@gmail.com>
2025-11-27 14:27:49 +0300

    Добавил строку подключения MySQL

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main()
         {
-            
+            //Строка подключения
+            string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
         }
     }
 }
