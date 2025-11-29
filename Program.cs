c7216fb4b7d6899c384971cf1c99f47ba9479039
Sergey Korolev <sergiussmithe@gmail.com>
2025-11-29 19:02:34 +0300

    Исправил строку подключения и подключился к базе данных

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
-﻿namespace StepikPetProject
+﻿using MySql.Data.MySqlClient;
+
+namespace StepikPetProject
 {
     class Program
     {
         static void Main()
         {
             //Строка подключения
-            string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
+            string connectionString = "Server=localhost;Database=stepik;Uid=root;Pwd=root;";
+
+            //Создаем подключение
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection open");
+
+            // Закрываем подключение
+            connection.Close();
+            Console.WriteLine("Подключение закрыто");
         }
     }
 }
