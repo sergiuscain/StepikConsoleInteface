6863f648c6acfc883b3d25dd0de6e7372d570f34
Sergey Korolev <sergiussmithe@gmail.com>
2025-12-03 12:35:24 +0300

    Создал папку для моделей данных и добавил первую модель - пользователя.

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Models
+{
+    public class User
+    {
+        public string FullName;
+        public string? Details;
+        public DateTime JoinDate = DateTime.Now;
+        public string? Avatar;
+        public bool IsActive = true;
+    }
+}
