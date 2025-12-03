b7f0ed7bbdc9dd1c2e0e2b5f16ecf0b40a8dbd21
Sergey Korolev <sergiussmithe@gmail.com>
2025-12-03 12:38:46 +0300

    Добавил класс Constant для временного хранения некоторых значений.

---
+++ Constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject
+{
+    public class Constant
+    {
+        //Временный класс для хранения константных значений
+        public const string ConnectionString = "Server=localhost;Database=stepik;Uid=root;Pwd=root;";
+    }
+}
