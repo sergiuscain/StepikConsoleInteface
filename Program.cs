0d6887ecc17ad7e33b2e3807fd6d88804ced381d
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-06 20:31:49 +0300

    Реализовал метод LoginUser, который проверяет, существует ли пользователь и если существует, выводит сообщение об успешной авторизации

--- Program.cs
+++ Program.cs
@@ -61,5 +61,19 @@ namespace StepikPetProject
                 Console.WriteLine("Произошла ошибка, произведен выход на главную страницу");
             }
         }
+        public static void LoginUser()
+        {
+            Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
+            string fullName = Console.ReadLine();
+            var user = UsersService.Get(fullName);
+            if (user != null)
+            {
+                Console.WriteLine($"Пользователь '{user.FullName}' успешно вошел\n");
+            }
+            else
+            {
+                Console.WriteLine("Пользователь не найден, произведен выход на главную страницу\n");
+            }
+        }
     }
 }
