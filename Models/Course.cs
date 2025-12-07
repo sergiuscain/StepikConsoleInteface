b3f22a40eb179c215d2c337746a3e5d572a6308c
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-07 09:50:38 +0300

    Создал модель курса на степик

---
+++ Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public string? Summary { get; set; }
+        public string? Photo {  get; set; }
+    }
+}
