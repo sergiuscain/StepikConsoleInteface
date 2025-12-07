ef0c79e0b3b0e86475918a97eafdcf82e3f65ad1
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-07 10:12:08 +0300

    Убрал создание таблицы в классе UsersService в методе Add. В данном проекте мы работаем с готовой базой данных, которая уже существует и создавать новые таблицы не нужно.

--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -18,22 +18,10 @@ namespace StepikPetProject.Services
                 using MySqlConnection connection = new MySqlConnection(Constant.ConnectionString);
                 connection.Open();
                 Console.WriteLine("Connection open");
-
-                // Создание таблицы
-                using MySqlCommand command = new MySqlCommand(@"
-                CREATE TABLE IF NOT EXISTS users (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    full_name VARCHAR(50) NOT NULL,
-                    details VARCHAR(50),
-                    join_date DATE NOT NULL,
-                    is_active BOOL,
-                    avatar TEXT
-                );", connection);
-                Console.WriteLine(command.ExecuteNonQuery());
                 // Вставка данных
-                command.CommandText = @"
+                using var command = new MySqlCommand(@"
                 INSERT INTO users (full_name, details, join_date, avatar, is_active) 
-                VALUES (@full_name, @details, @join_date, @avatar, @is_active);";
+                VALUES (@full_name, @details, @join_date, @avatar, @is_active);", connection);
                 command.Parameters.AddWithValue("@full_name", user.FullName);
                 command.Parameters.AddWithValue("@details", user.Details);
                 command.Parameters.AddWithValue("@join_date", user.JoinDate);
