5d0f1324d59d74a7135128b0d3bcc967acd7318a
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-07 09:58:25 +0300

    Реализовал метод GetTotalCount для получения общего количества курсов. И создал класс CoursesService для работы с курсами.

---
+++ Services/CoursesService.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Services
+{
+    public class CoursesService
+    {
+        /// <summary>
+        /// Получение общего количества курсов
+        /// </summary>
+        public static int GetTotalCount()
+        {
+            using var connection = new MySqlConnection(Constant.ConnectionString);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT COUNT(*) FROM courses;", connection);
+            var result = command.ExecuteScalar();
+            return result == null ? 0 : (int)result;
+        }
+    }
+}
