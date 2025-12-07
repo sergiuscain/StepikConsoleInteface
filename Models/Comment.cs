b6c97078f6686abff8306b087a1067ab6d2d0e94
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-07 19:34:52 +0300

    Добавил модель комментариев

---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
