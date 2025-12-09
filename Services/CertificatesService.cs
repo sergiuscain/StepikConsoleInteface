ea1b9bea4d5e5d42ee3bfeafefc33323adaa3034
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-09 20:40:42 +0300

    Добавил сервис для работы сертификатов и метод, который получает сертификат пользователя по имени

---
+++ Services/CertificatesService.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Services
+{
+    public class CertificatesService
+    {
+        /// <summary>
+        /// Получение сертификатов пользователя
+        /// </summary>
+        /// <param name="fullName">Полное имя пользователя</param>
+        /// <returns>DataSet</returns>
+        public static DataSet Get(string fullName)
+        {
+            // Создаем подключение, DataSet, SQL запрос, объект команды и DataAdapter
+            using var connection = new MySqlConnection(Constant.ConnectionString);
+            var dataSet = new DataSet();
+            string sqlQuery = $@"SELECT courses.title, certificates.issue_date, certificates.grade
+                                FROM certificates
+                                JOIN users ON certificates.user_id = users.id
+                                JOIN courses ON certificates.course_id = courses.id
+                                WHERE users.full_name = @fullName
+                                ORDER BY certificates.issue_date DESC;";
+            using var command = new MySqlCommand(sqlQuery, connection);
+            // Имя передается через параметр команды, что бы защитить от инъекции чужого SQL кода
+            command.Parameters.AddWithValue("@fullName", fullName);
+            using var dataAdapter = new MySqlDataAdapter(command);
+            // Заполняем DataSet
+            dataAdapter.Fill(dataSet);
+            return dataSet;
+        }
+    }
+}
