c0a1a137d04342bb6df84aac087f9e51563c982d
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-09 20:10:09 +0300

    Создал модель сертификата

---
+++ Models/Certificate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepikPetProject.Models
+{
+    public class Certificate
+    {
+        public string Title { get; set; }
+        public DateTime IssueDate { get; set; }
+        public int Grade { get; set; }
+    }
+}
